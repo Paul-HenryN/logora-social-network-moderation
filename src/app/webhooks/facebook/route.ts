86a4c1921050b4484de2f43c6b9e217a46931ea6
paul-henryN <ngpaulhenry@gmail.com>
2024-03-20 11:35:38 +0100

    feat: add facebook webhook endpoints

---
+++ src/app/webhooks/facebook/route.ts
@@ -0,0 +1,19 @@
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+
+  const mode = searchParams.get("hub.mode");
+  const challenge = searchParams.get("hub.challenge");
+  const verifyToken = searchParams.get("hub.verify_token");
+
+  if (mode === "subscribe" && verifyToken === "logora") {
+    return new Response(challenge);
+  }
+
+  return new Response("error", { status: 400 });
+}
+
+export async function POST(request: Request) {
+  console.log(request);
+
+  return new Response("success", { status: 200 });
+}
